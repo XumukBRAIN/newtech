fcf43e88ea38000b2fcc97a1b37b33d291a56db2
XumukBRAIN <xumuk_332@mail.ru>
2023-05-22 21:23:09 +0300

    start: Кодирование пароля, principal и TokenDetails

---
+++ src/main/java/ru/kudryashov/newtech/security/TokenDetails.java
@@ -0,0 +1,20 @@
+package ru.kudryashov.newtech.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDetails {
+    private UUID userId;
+    private String token;
+    private Date issuedAt;
+    private Date expiresAt;
+}
